5be3af2920c16e40d1942b73600fdf8d44cfc8d5
ZiMing <wwgg012345@gmail.com>
2020-06-15 03:59:59 +0800

    完成 測試 多視差滾動

--- scrpit/Js/scroll_anime.js
+++ scrpit/Js/scroll_anime.js
@@ -21,3 +21,99 @@ var index_scroll = new ScrollMagic.Scene({
 .setPin(".index")
 .setTween(scroll_anime)	
 .addTo(controller);
+
+var index_pall = new TimelineMax()
+	
+	.add([
+			TweenMax.to(".index_lee_img" , 1 , {right: "15%"}),
+		]);
+
+
+
+var index = new ScrollMagic.Scene({
+	
+	triggerElement:'.index',
+	
+	triggerHook:'onLeave',
+	
+	duration:'50%',
+	
+})
+
+.setTween(index_pall)
+.addIndicators()
+.addTo(controller);
+
+
+
+var info_pall = new TimelineMax()
+	
+	.add([
+			TweenMax.to(".index_lee_img2" , 1 , {right: "15%", delay:2}),
+		]);
+
+
+
+var info = new ScrollMagic.Scene({
+	
+	triggerElement:'.index',
+	
+	triggerHook:'onLeave',
+	
+	duration:'250%',
+	
+})
+
+.setTween(info_pall)
+.addIndicators()
+.addTo(controller);
+
+var write_pall = new TimelineMax()
+	
+	.add([
+			TweenMax.to(".index_lee_img3" , 1 , {right: "15%", delay:4}),
+		]);
+
+
+
+var write = new ScrollMagic.Scene({
+	
+	triggerElement:'.index',
+	
+	triggerHook:'onLeave',
+	
+	duration:'400%',
+	
+})
+
+.setTween(write_pall)
+.addIndicators()
+.addTo(controller);
+
+
+
+
+var effect_pall = new TimelineMax()
+	
+	.add([
+			TweenMax.to(".index_lee_img4" , 1 , {right: "15%", delay:8}),
+		]);
+
+
+
+var effect = new ScrollMagic.Scene({
+	
+	triggerElement:'.index',
+	
+	triggerHook:'onLeave',
+	
+	duration:'500%',
+	
+})
+
+.setTween(effect_pall)
+.addIndicators()
+.addTo(controller);
+
+
+
