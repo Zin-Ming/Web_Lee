aa8097539ed9e69127e4450a5ab6eed8a3aad095
ZiMing <wwgg012345@gmail.com>
2020-06-20 02:55:25 +0800

    完成 .write vue 動態改變文字

--- scrpit/vue/web_vue.js
+++ scrpit/vue/web_vue.js
@@ -6,10 +6,12 @@ var write = new Vue({
 	
 	data:{
 		
-		show_01:false,
-		show_02:false,
-		show_03:false,
-		show_04:false,
+		title:'',
+		
+		p1:'',
+		p2:'',
+		p3:'',
+		p4:'',
 		
 	},
 	
@@ -20,24 +22,45 @@ var write = new Vue({
 			
 			if(number === 1){
 				
-				this.show_01 = true;
+				this.title = '錦瑟';
+				
+				this.p1 = '錦瑟無端五十弦，一弦一柱思華年。';
+				this.p2 = '庄生曉夢迷蝴蝶，望帝春心托杜鵑。';
+				this.p3 = '滄海月明珠有淚，藍田日暖玉生煙。';
+				this.p4 = '此情可待成追憶，只是當時已惘然。';
+				
 				
 			}else if(number === 2){
 				
-				this.show_02 = true;	
+				this.title = '夜雨寄北';
+				
+				this.p1 = '君問歸期未有期，';
+				this.p2 = '巴山夜雨漲秋池。';
+				this.p3 = '何當共剪西窗燭，';
+				this.p4 = '卻話巴山夜雨時。';
+					
 				
 			}else if(number === 3){
 				
-				this.show_03 = true;	
+				this.title = '無題';
+				
+				this.p1 = '相見時難別亦難，東風無力百花殘。';
+				this.p2 = '春蚕到死絲方盡，蜡炬成灰淚始干。';
+				this.p3 = '曉鏡但愁云鬢改，夜吟應覺月光寒。';
+				this.p4 = '蓬山此去無多路，青鳥殷勤為探看。';
 				
 			}else{
 				
-				this.show_04 = true;
+				this.title = '悼傷後赴東蜀辟至散關遇雪';
+				
+				this.p1 = '劍外從軍遠，';
+				this.p2 = '無家与寄衣。';
+				this.p3 = '散關三尺雪，';
+				this.p4 = '回夢舊鴛机。';
+				
 			}
 			
-		},
-		
-		
+		}
 		
 	},
 	
